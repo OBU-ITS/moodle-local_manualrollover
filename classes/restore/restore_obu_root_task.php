2fb68b0d2e9d2a62ab5e357e16322c2e395fe09f
Jock Coats <p0071665@brookes.ac.uk>
2025-07-24 14:56:04 +0100

    Add step to unconditionally restore course section hierarchy and names

--- classes/restore/restore_obu_root_task.php
+++ classes/restore/restore_obu_root_task.php
@@ -36,6 +36,9 @@ class restore_obu_root_task extends \restore_root_task {
         // Unconditionally, load create all the needed groups and groupings
         $this->add_step(new \local_manualrollover\restore\restore_obu_groups_structure_step('create_obu_groups_and_groupings', 'groups.xml'));
 
+        // Unconditionally load section hierarchy and names
+        $this->add_step(new \restore_course_sections_structure_step('course_sections', 'sections.xml'));
+
         // Unconditionally, load create all the needed scales
         $this->add_step(new \restore_scales_structure_step('create_scales', 'scales.xml'));
 
