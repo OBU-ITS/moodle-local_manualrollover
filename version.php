fb2113057f5572343115adddefb43f93527379cb
Jock Coats <p0071665@brookes.ac.uk>
2025-07-25 01:53:16 +0100

    - Add error handling for `restore_course_sections_structure_step` to catch exceptions and throw a `moodle_exception` with detailed information - Increment plugin version from 2025072501 to 2025072502

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_manualrollover';
-$plugin->version = 2025072501;
+$plugin->version = 2025072502;
 $plugin->requires = 2012120301;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.3.1';
