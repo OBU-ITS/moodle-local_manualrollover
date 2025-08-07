41d0f5a17d3bd4bb2cdcab2161d95331a08c4356
Jock Coats <p0071665@brookes.ac.uk>
2025-08-07 20:52:02 +0100

    Update plugin version and release
    Incremented version number to 2025080700 and release to 'v1.4.0'.

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_manualrollover';
-$plugin->version = 2024111101;
+$plugin->version = 2025080700;
 $plugin->requires = 2012120301;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.3.0';
+$plugin->release = 'v1.4.0';
 $plugin->dependencies = array(
     'local_obu_group_manager' => 2024081202
 );
