d5c263f67c5882928e02ade9c03455c7d50792b5
Joe Souch <p0091841@brookes.ac.uk>
2025-08-08 10:37:18 +0100

    version bump

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_manualrollover';
 
-$plugin->version = 2024102501;
+$plugin->version = 2025072809;
 $plugin->requires = 2012120301;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v1.4.0';
